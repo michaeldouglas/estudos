096bcc7222531ccd8c34f9f510db1c454a8d6cf6
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-09-02 00:32:23 -0300

    Estudos

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-cpmponent',
+    template: `
+        <p>Meu primeiro componet com Angular2!</p>
+    `
+})
+export class MeuPrimeiroComponent 
+{
+
+}
