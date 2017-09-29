17dee7d2d8c09f4f629788d7a25436513df8a25f
Michael Douglas <michaeldouglas010790@gmail.com>
2017-09-29 01:09:43 -0300

    Templates
    Estudos sobre templates.

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext js', 'Teste'];
+
+  constructor() {
+    this.nomePortal = 'http://mdbaweb.com.br';
+  }
 
   ngOnInit() {
   }
