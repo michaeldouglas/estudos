a9e4840e602d3ae2906abf9b6090f95402ea95ea
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-11-10 14:43:49 -0200

    Estudos

---
+++ protractor/e2e/protractor.conf.js
@@ -0,0 +1,8 @@
+module.exports.config	=	{
+    seleniumAddress:	'http://localhost:4444/wd/hub',
+    capabilities:	{
+            'browserName':	'chrome'
+    },
+    specs:	['specs/*.spec.js'],
+    baseUrl:	'http://www.protractortest.org/'
+};
